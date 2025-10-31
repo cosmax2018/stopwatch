ab7b3028b5deed217284f10db1d1d97bbfc8a6bc
Massimiliano Cosmelli <massimiliano.cosmelli@accelleron-industries.com>
2025-10-31 07:34:31 +0100

    Rimuovi avvio_stopwatch.bat dal repository e aggiunto a .gitignore

--- avvio_stopwatch.bat
+++
@@ -1,3 +0,0 @@
-REM lancia stopwatch.py all'avvio di Windows senza finestra terminale
-@echo off
-"C:\Users\ITMACOS\AppData\Local\Programs\Python\Python313\pythonw.exe" "C:\Users\ITMACOS\stopwatch\stopwatch.py"
